1077e30b938ac9875de80db1989efe93a69331c1
Lokesh Patel <patellokesh05899@gmail.com>
2021-03-16 21:12:14 +0530

    Added the system call names array to defs.h

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -155,6 +155,7 @@ int             argstr(int, char**);
 int             fetchint(uint, int*);
 int             fetchstr(uint, char**);
 void            syscall(void);
+extern char*    syscallnames[];
 extern bool     trace_syscall;
 extern uint     count_syscall[100];
 
