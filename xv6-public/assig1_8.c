c935a03a99dfc9dbe55baadaaa5ac5798c83494e
Lokesh Patel <patellokesh05899@gmail.com>
2021-03-17 18:37:55 +0530

    Completed the distributed algorithm part

--- xv6-public/assig1_8.c
+++ xv6-public/assig1_8.c
@@ -30,17 +30,72 @@ main(int argc, char *argv[])
   	printf(1,"first elem %d\n", arr[0]);
   
   	//----FILL THE CODE HERE for unicast sum
-
-
-
-
-
-
-
-
-
-
-
+	int parPID = getpid();
+	int max_process = 8;
+	int section_size = (size+max_process-1)/max_process; //ceil(size/max_process)
+	int childPID;
+	char *msg = (char*)malloc(8);
+	//debug -----------------------------------------------------------------------------
+  	for(int i=0;i<max_process;++i){
+  		int s=i*section_size, e=(i+1)*section_size;
+  		if(e>size) e=size;
+  		int sum=0;
+  		for(int i=s;i<e;++i){
+  			sum += arr[i];
+  		}
+  		printf(1,"i=%d, sum=%d\n", i, sum);
+  	}
+  	//debug end --------------------------------------------------------------------------
+	for(int i=1; i<max_process; ++i){
+		childPID = fork();
+		if(childPID == 0){
+			break;
+		}else{
+			*msg = '0'+i;
+			*(msg+1) = 0;
+			send(parPID, childPID, msg);
+		}
+	}
+	if(childPID == 0){
+		recv(msg);
+		int section = atoi(msg);
+		int start = section*section_size, end = (section+1)*section_size;
+		if(end > size) end = size;
+		int sum=0;
+		for(int i=start;i<end;++i){
+			sum += arr[i];
+		}
+		char *t = msg;
+		int sz=0;
+		while(sum > 0){
+			++sz;
+			*t++ = '0'+sum%10;
+			sum /= 10;
+		}
+		*t=0;
+		//Now we need to reverse the string as we have stored LSB in the beginning
+		for(int i=0;i<sz/2;++i){
+			char temp = msg[i];
+			msg[i] = msg[sz-1-i];
+			msg[sz-1-i] = temp;
+		}
+		send(getpid(), parPID, msg);
+		free(msg);
+		exit();
+	}else{
+		for(int i=0;i<section_size;++i){
+			tot_sum += arr[i];
+		}
+		for(int i=1;i<max_process;++i){
+			wait(); //Wait till all other processes complete the summation
+		}
+		for(int i=1;i<max_process;++i){
+			recv(msg); //Recieve the sum from one of the child
+			int sum = atoi(msg);
+			tot_sum += sum;
+		}
+		free(msg);
+	}
   	//------------------
 
   	if(type==0){ //unicast sum
